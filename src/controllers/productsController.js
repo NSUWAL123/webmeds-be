4f71fe9d4da48497ca77ed81619036421c0bb253
Nishit Suwal <suwalnishit@gmail.com>
2023-01-19 18:43:07 +0545

    Added endpoint and function for getting individual product

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,6 +6,12 @@ const getAllProducts = async (req, res) => {
     res.json(products);
 }
 
+const getProductByName = async (req, res) => {
+    const product = await Product.findOne({pname: req.params.pname})
+
+    res.json(product)
+}
+
 const medicine = (req, res) => {
     res.send("In medicines");
 }
@@ -18,4 +24,4 @@ const personalcare = (req, res) => {
     res.send("In personalcare");
 }
 
-module.exports = {getAllProducts, medicine, baby, personalcare};
+module.exports = {getAllProducts, medicine, baby, personalcare, getProductByName};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getAllProducts, medicine, baby, personalcare} = require('../controllers/productsController')
+const {getAllProducts, getProductByName, medicine, baby, personalcare} = require('../controllers/productsController')
 const authUser = require('../middlewares/authUser')
 
 router.get('/', getAllProducts)
+router.get('/:pname', getProductByName)
 router.get('/medicines', authUser, medicine);
 router.get('/baby', authUser, baby);
 router.get('/personalcare', personalcare);
