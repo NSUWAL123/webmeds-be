8822912973def42702042341508d5627ab49bb41
Nishit Suwal <suwalnishit@gmail.com>
2023-02-10 13:19:38 +0545

    Manage profile completed

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,54 +1,54 @@
-const Note = require("../models/noteModel")
+const Note = require("../models/noteModel");
 
 const getAllNotes = async (req, res) => {
-    const getNotes = await Note.find({
-        userId: req.user.id
-    })
+  const getNotes = await Note.find({
+    userId: req.user.id,
+  });
 
-    res.json({getNotes});
-}
+  res.json({ getNotes });
+};
 
 const addNotes = async (req, res) => {
-    const userId = req.user.id;
-    const {title, description} = req.body;
-    console.log(userId, title, description)
+  const userId = req.user.id;
+  const { title, description } = req.body;
+  console.log(userId, title, description);
 
-    const newNote = await Note.create({
-        userId: userId,
-        title: title,
-        description: description
-    })
+  const newNote = await Note.create({
+    userId: userId,
+    title: title,
+    description: description,
+  });
 
-    const noteId = newNote._id;
+  const noteId = newNote._id;
 
-    res.json({
-        _id: noteId,
-        userId,
-        title,
-        description
-    })
-}
+  res.json({
+    _id: noteId,
+    userId,
+    title,
+    description,
+  });
+};
 
 const updateNotes = async (req, res) => {
-    const {title, description} = req.body;
-    const updateNote = await Note.findByIdAndUpdate(req.params.id, {
-        title: title,
-        description: description
-    })
+  const { title, description } = req.body;
+  const updateNote = await Note.findByIdAndUpdate(req.params.id, {
+    title: title,
+    description: description,
+  });
 
-    res.json("Update successfully.")
-}
+  res.json("Update successfully.");
+};
 
 const deleteNotes = async (req, res) => {
-    const deleteNote = await Note.findByIdAndDelete(req.params.id)
+  const deleteNote = await Note.findByIdAndDelete(req.params.id);
 
-    const title = deleteNote.title;
-    res.send(`Note with titel: ${title} is successfully deleted.`)
-}
+  const title = deleteNote.title;
+  res.send(`Note with titel: ${title} is successfully deleted.`);
+};
 
 module.exports = {
-    getAllNotes,
-    addNotes,
-    updateNotes,
-    deleteNotes
-}
+  getAllNotes,
+  addNotes,
+  updateNotes,
+  deleteNotes,
+};
