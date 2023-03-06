95c453cd81943e17b14ffe853f9c971aa0025186
Nishit Suwal <suwalnishit@gmail.com>
2023-03-06 18:34:26 +0545

    Khalti integration updated

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,30 +1,49 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const initiatePayment = async (req, res) => {
-    const {token, amount} = req.body;
+  const { token, amount } = req.body;
 
-    let data = {
-        "token": token,
-        "amount": amount,
-      };
+  let data = {
+    token: token,
+    amount: amount,
+  };
 
-      console.log(data)
-      
-      let config = {
-        headers: {'Authorization': `Key ${process.env.KHALTI_SECRET_KEY}`}
-      };
+  console.log(data);
 
-      console.log(config)
-      
-      axios.post("https://khalti.com/api/v2/payment/verify/", data, config)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        // console.log(error);
+  let config = {
+    headers: { Authorization: `Key ${process.env.KHALTI_SECRET_KEY}` },
+  };
 
-        console.log("error was found")
+  console.log(config);
+
+  try {
+    const response = await axios.post(
+      "https://khalti.com/api/v2/payment/verify/",
+      data,
+      config
+    );
+    console.log(response)
+    if (response) {
+      res.json({
+        data: response,
+        paid: true,
       });
-}
+    }
+  } catch (error) {
+    console.log("error was found" + error);
+  }
 
-module.exports = {initiatePayment};
+  const response = await axios
+    .post("https://khalti.com/api/v2/payment/verify/", data, config)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      // console.log(error);
+
+      console.log("error was found");
+    });
+  res.send(response);
+};
+
+module.exports = { initiatePayment };
